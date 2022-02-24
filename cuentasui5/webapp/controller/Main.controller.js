7e29e97dbf886f121db0f6f4aadedb5253c45fd2
eduardoguerrerob <eduardoguerrerob@gmail.com>
2022-02-24 01:45:59 +0000

    20220223:step2 incomplete

--- cuentasui5/webapp/controller/Main.controller.js
+++ cuentasui5/webapp/controller/Main.controller.js
@@ -10,30 +10,43 @@ sap.ui.define([
         "use strict";
 
         const paramModel = new JSONModel();
+        const chartsModel = new JSONModel();
 
         function onInit() {
             paramModel.loadData("./localService/mockdata/params.json", "false");
             this.getView().setModel(paramModel, "paramModel");
+
+            chartsModel.loadData("./localService/mockdata/chartOfAccount.json", "false");
+            this.getView().setModel(chartsModel, "chartsModel");
         }
 
         function onValidateStep1(oEvent){
             const name = this.getView().byId("inp1").getValue();
-            if( name != '' && name != undefined){
+            const description = this.getView().byId("txa1").getValue();
+            if( name != '' && name != undefined && description != '' && description != undefined){
                 // actualizar paramModel
                 paramModel.setProperty("/employeeName", name);
+                paramModel.setProperty("/description", description);
+
                 const steps = this.getView().byId("CreateProductWizard").getSteps();
                 steps[0].setValidated(true);
             }
             else{
-                paramModel.setProperty("/employeeName", "");
+                // paramModel.setProperty("/employeeName", "");
+                // paramModel.setProperty("/description", "");
                 const steps = this.getView().byId("CreateProductWizard").getSteps();
                 steps[0].setValidated(false);
             }
         }
 
+        function onValidateChart(){
+
+        }
+
         const Main = Controller.extend("egb.cuentasui5.controller.Main", {});
         Main.prototype.onInit = onInit;
         Main.prototype.onValidateStep1 = onValidateStep1;
+        Main.prototype.onValidateChart = onValidateChart;
 
         return Main
     });
